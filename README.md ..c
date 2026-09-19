45597ca2c9d22ddcb20e2750e52f4e4015ba9550
nisha18-prog <nisha.1497p@gmail.com>
2026-09-19 14:46:19 +0530

    README file
    study website

---
+++ README.md ..c
@@ -0,0 +1,17 @@
+# 📚 Study Plan Website
+
+A simple study planning website created using HTML.
+
+## Features
+- Daily study goals
+- Timetable
+- Subject-wise planning
+- Motivational section
+
+## Technologies Used
+- HTML
+- GitHub Pages
+
+## Purpose
+This project was created as part of my GitHub activity to learn
+how to create and publish a basic website.
